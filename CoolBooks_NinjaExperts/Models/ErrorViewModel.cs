5203a379859772b9543a72153fdddf44d134423e
ERoyson <94379543+ERoyson@users.noreply.github.com>
2022-04-04 12:02:44 +0200

    Add project files.

---
+++ CoolBooks_NinjaExperts/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CoolBooks_NinjaExperts.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
