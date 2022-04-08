d1ea2b3fd4f1b04d8d708f2437a5ba71753f3859
ERoyson <94379543+ERoyson@users.noreply.github.com>
2022-04-08 13:49:20 +0200

    Created script for genres and authors, images and thumbnail handeling, Identity and authentication/authorization (start)

--- CoolBooks_NinjaExperts/Models/Books.cs
+++ CoolBooks_NinjaExperts/Models/Books.cs
@@ -1,4 +1,5 @@
 ﻿using CoolBooks_NinjaExperts.Areas.Identity.Data;
+using System.ComponentModel.DataAnnotations;
 
 namespace CoolBooks_NinjaExperts.Models
 {
@@ -6,15 +7,35 @@ namespace CoolBooks_NinjaExperts.Models
     {
         public int Id { get; set; }
         public UserInfo User { get; set; } // Fk UserId - AspNetUsers
+
+        [Required]
+        [StringLength(255, ErrorMessage = "The Book-Title must be less than 255 characters")]
         public string Title { get; set; }
+
+        [Required]
         public string Description { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime? Published { get; set; }
+
+        [Required]
         public long ISBN { get; set; }
-        public double Rating { get; set; }
-        public Images Image { get; set; } // FK ImageId 
+
+        public double? Rating { get; set; }
+
+        public Images? Image { get; set; } // FK ImageId 
+
         public DateTime Created { get; set; }
-        public DateTime Deleted { get; set; }
+
+        public DateTime? Deleted { get; set; }
 
         public virtual ICollection<Genres> Genres { get; set; } // many to many relationship
         public virtual ICollection<Authors> Authors { get; set; } // many to many relationship
+
+        public Books()
+        {
+            this.Created = DateTime.Now;
+        }
     }
 }
