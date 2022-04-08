f84c9e6aaaa1871da980a2f5f02031c39323f308
mathiasjohansson84 <mathiasjohansson84@gmail.com>
2022-04-08 16:09:26 +0200

    searchfunktion

--- CoolBooks_NinjaExperts/Models/Books.cs
+++ CoolBooks_NinjaExperts/Models/Books.cs
@@ -5,14 +5,19 @@ namespace CoolBooks_NinjaExperts.Models
     public class Books
     {
         public int Id { get; set; }
-        public UserInfo User { get; set; } // Fk UserId - AspNetUsers
+        public UserInfo? User { get; set; } // Fk UserId - AspNetUsers
         public string Title { get; set; }
         public string Description { get; set; }
         public long ISBN { get; set; }
         public double Rating { get; set; }
-        public Images Image { get; set; } // FK ImageId 
-        public DateTime Created { get; set; }
-        public DateTime Deleted { get; set; }
+        public Images? Image { get; set; } // FK ImageId 
+        public DateTime? Created { get; set; }
+        public DateTime? Deleted { get; set; }
+
+        public Books()
+        {
+            this.Created = DateTime.Now;
+        }
 
         public virtual ICollection<Genres> Genres { get; set; } // many to many relationship
         public virtual ICollection<Authors> Authors { get; set; } // many to many relationship
