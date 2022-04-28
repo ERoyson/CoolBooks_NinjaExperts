8fc18b6b6da7c82c598ffc3105d4cb8848acef13
cubaz237 <cubaz16@gmail.com>
2022-04-28 15:50:45 +0100

    Quiz table added to DB

---
+++ CoolBooks_NinjaExperts/Models/Questions.cs
@@ -0,0 +1,15 @@
+﻿namespace CoolBooks_NinjaExperts.Models
+{
+    public class Questions
+    {
+        public int Id { get; set; }
+        public Quiz Quiz { get; set; } //FK QuizId
+        public string Question { get; set; }
+        public string OptionA { get; set; }
+        public string OptionB { get; set; }
+        public string OptionC { get; set; }
+        public string OptionD { get; set; }
+        public string Answer { get; set; }
+
+    }
+}
