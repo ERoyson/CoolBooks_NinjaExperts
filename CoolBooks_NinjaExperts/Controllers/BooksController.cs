39ec51aa21d02caa94758bfa9252891c282c8161
ERoyson <94379543+ERoyson@users.noreply.github.com>
2022-04-29 11:28:58 +0200

    added authors

--- CoolBooks_NinjaExperts/Controllers/BooksController.cs
+++ CoolBooks_NinjaExperts/Controllers/BooksController.cs
@@ -132,6 +132,7 @@ namespace CoolBooks_NinjaExperts.Models
 
         // GET: Books/Details/5
         //[Authorize(Roles = "Admin, Moderator, User")]
+        
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
