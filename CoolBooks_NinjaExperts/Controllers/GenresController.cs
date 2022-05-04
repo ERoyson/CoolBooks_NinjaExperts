e21fda4137669ad7b989226742bea6632bc5f9f4
Pernilla Skoglund <pearch@hotmail.com>
2022-05-04 09:45:54 +0200

    Create och Edit Genres

--- CoolBooks_NinjaExperts/Controllers/GenresController.cs
+++ CoolBooks_NinjaExperts/Controllers/GenresController.cs
@@ -52,8 +52,7 @@ namespace CoolBooks_NinjaExperts.Models
         }
 
         // POST: Genres/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,Name,Description,Created")] Genres genres)
